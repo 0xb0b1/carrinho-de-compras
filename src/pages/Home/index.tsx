d3cefcc43e37c84522b7b6fd9e0b001deb1799af
Paulo Vicente <vcente82@gmail.com>
2022-01-31 10:43:47 -0300

    home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -22,51 +22,47 @@ interface CartItemsAmount {
 }
 
 const Home = (): JSX.Element => {
-  const [products, setProducts] = useState<ProductFormatted[]>([]);
-  const { addProduct, cart } = useCart();
+  // const [products, setProducts] = useState<ProductFormatted[]>([]);
+  // const { addProduct, cart } = useCart();
 
-  const cartItemsAmount = cart.reduce((sumAmount, product) => {
-    sumAmount[product.id] = product.amount;
-    return sumAmount;
-  }, {} as CartItemsAmount);
+  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
+  //   // TODO
+  // }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
-      const response = await api.get("/products");
-      const productsFormated = response.data.map(function (product: Product) {
-        return { ...product, price: formatPrice(product.price) };
-      });
-      setProducts(productsFormated);
+      // TODO
     }
 
     loadProducts();
   }, []);
 
   function handleAddProduct(id: number) {
-    addProduct(id);
+    // TODO
   }
 
   return (
     <ProductList>
-      {products.map((product) => (
-        <li key={product.id}>
-          <img src={product.image} alt={product.title} />
-          <strong>{product.title}</strong>
-          <span>{product.price}</span>
-          <button
-            type="button"
-            data-testid="add-product-button"
-            onClick={() => handleAddProduct(product.id)}
-          >
-            <div data-testid="cart-product-quantity">
-              <MdAddShoppingCart size={16} color="#FFF" />
-              {cartItemsAmount[product.id] || 0}
-            </div>
+      <li>
+        <img
+          src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg"
+          alt="Tênis de Caminhada Leve Confortável"
+        />
+        <strong>Tênis de Caminhada Leve Confortável</strong>
+        <span>R$ 179,90</span>
+        <button
+          type="button"
+          data-testid="add-product-button"
+          // onClick={() => handleAddProduct(product.id)}
+        >
+          <div data-testid="cart-product-quantity">
+            <MdAddShoppingCart size={16} color="#FFF" />
+            {/* {cartItemsAmount[product.id] || 0} */} 2
+          </div>
 
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-      ))}
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
     </ProductList>
   );
 };
